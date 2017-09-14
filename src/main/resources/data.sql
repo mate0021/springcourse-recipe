820f2edb52a6aff24bbb3a351612732b9e05bf59
mate00 <mami.ext@gmail.com>
2017-09-14 20:45:56 +0200

    Added init data script.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (name) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (name) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (name) VALUES ('Cup');
+INSERT INTO unit_of_measure (name) VALUES ('Pinch');
+INSERT INTO unit_of_measure (name) VALUES ('Ounce');
