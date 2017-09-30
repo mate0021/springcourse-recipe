5588367113d436afd0a22c2a2af367a4c4e38036
mate00 <mami.ext@gmail.com>
2017-09-30 12:47:49 +0200

    Creating a new Recipe from html form.

--- src/main/java/rnd/mate00/springboot/recipe/controllers/RecipeController.java
+++ src/main/java/rnd/mate00/springboot/recipe/controllers/RecipeController.java
@@ -3,8 +3,8 @@ package rnd.mate00.springboot.recipe.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import rnd.mate00.springboot.recipe.commands.RecipeCommand;
 import rnd.mate00.springboot.recipe.service.RecipeService;
 
 /**
@@ -27,5 +27,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(recipeId));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipeForm")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) { // will find form to match RecipeCommand
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId(); // just after creating will redirect to newly saved Recipe
     }
 }
