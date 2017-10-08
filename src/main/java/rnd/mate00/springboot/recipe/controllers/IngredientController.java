ff0ad30d664e184e55882a5551d246045d8b6b06
mate00 <mami.ext@gmail.com>
2017-10-08 13:16:31 +0200

    Show ingredients and navigate back to recipe.

--- src/main/java/rnd/mate00/springboot/recipe/controllers/IngredientController.java
+++ src/main/java/rnd/mate00/springboot/recipe/controllers/IngredientController.java
@@ -3,7 +3,6 @@ package rnd.mate00.springboot.recipe.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import rnd.mate00.springboot.recipe.model.Ingredient;
